43fbca80822a1d19d25f6f8e1eee73e88167f79f
DWilliams904 <dominiquewilliams3227@gmail.com>
2022-10-30 18:31:34 -0700

    added error handeling and timestamp to loginscript also uploaded backup

--- loginscript.py
+++ loginscript.py
@@ -1,6 +1,9 @@
 import os
 import netmiko
 from netmiko import ConnectHandler
+from netmiko.ssh_exception import NetMikoTimeoutException
+from netmiko.ssh_exception import AuthenticationException
+from netmiko.ssh_exception import SSHException
 from getpass import getpass
 from datetime import date
 
@@ -17,14 +20,25 @@ router = {
         "password" : pw,
         "device_type" : "cisco_ios"
 }        
-        
-ch = ConnectHandler(**router)
-        
-        
-conf = ch.send_command("show run")
-        
-f = open(f"ConfigSnap.config{date}" , "x")
+
+try:        
+	ch = ConnectHandler(**router)
+	conf = ch.send_command("show run")
+	f = open(f"ConfigSnap.config{date}" , "x")
+	f.write(conf)
+	f.close
+	
+	
+except (NetMikoTimeoutException):
+	print("The following device has timed out: " + router["ip"])
+except (AuthenticationException):
+	print("Authentication failure on device: " + router["ip"])
+except(SSHException):
+	print("Could not connect to device via SSH. Please check SSH settings on: " + router["ip"])		
+	
+print("The script has completed")
 
 
-f.write(conf)
-f.close
+
+
+
